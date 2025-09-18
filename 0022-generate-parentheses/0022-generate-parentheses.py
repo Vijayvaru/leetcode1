e0a8c398a71aced3f2be2c84a9bbb9b1a1783573
Vijayvarun chembolu <155285254+Vijayvaru@users.noreply.github.com>
2025-09-18 09:04:30 +0530

    Time: 0 ms (100%), Space: 12.8 MB (12.83%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n):
+        res = []
+        
+        def backtrack(s, left, right):
+            if len(s) == 2 * n:
+                res.append(s)
+                return
+            
+            if left < n:
+                backtrack(s + '(', left + 1, right)
+            if right < left:
+                backtrack(s + ')', left, right + 1)
+        
+        backtrack('', 0, 0)
+        return res
+
+# Example usage:
+sol = Solution()
+print(sol.generateParenthesis(3))
